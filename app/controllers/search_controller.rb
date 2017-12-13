9c3fc443d9a721105115da5d14925870d0347e59
Aaron Schreck <schreck.aaron@gmail.com>
2017-12-13 09:41:22 -0700

    Making faraday calls in the controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,10 @@
 class SearchController < ApplicationController
 
   def index
-    
+    #an array of station objects needs to come out of here. 
+    #I have the zip code
+    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&location=#{params[:q]}&radius=6.0&api_key=RcMCcV7bXdBRp4r5Vgh4FZR5jvwGqBJgPgfG9rQN&format=JSON")
+    byebug
+    #this returns a blob of JSON. 
   end 
 end 
