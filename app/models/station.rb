d1cf09ddb7256ec3fdee43c6e081755dc23a2b22
Aaron Schreck <schreck.aaron@gmail.com>
2017-12-13 09:51:54 -0700

    add station PORO

---
+++ app/models/station.rb
@@ -0,0 +1,11 @@
+class Station 
+
+  def initialize(info)
+    @name = info["station_name"]
+    @address = info["street_address"]
+    @fuel_type = info["fuel_type_code"]
+    @distance= info["distance"]
+    @access_times = info["access_days_time"]
+  end 
+
+end 
