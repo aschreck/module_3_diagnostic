0a3b4fbe8cc2ef7129658b78afa2e83e8e08d1ce
Aaron Schreck <schreck.aaron@gmail.com>
2017-12-13 10:55:34 -0700

    Removed more comments, made header of search page display the input zip code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,6 @@ class SearchController < ApplicationController
 
   def index
     @stations = DevnetService.get_nearby_stations(params[:q])
+    @location = params[:q]
   end 
 end 
--- spec/features/user_can_see_stations_by_zip_code_spec.rb
+++ spec/features/user_can_see_stations_by_zip_code_spec.rb
@@ -2,20 +2,14 @@ require 'rails_helper'
 
 describe "When a user inputs their zip code into the search" do
   it "return a list of nearby stations with criteria and information" do
-    # As a user
-    # When I visit "/"
     visit '/'
-    # And I fill in the search form with 80203
-    fill_in "q", with: 80203
-    # And I click "Locate"
-    click_on "Locate"
-    # Then I should be on page "/search" with parameters visible in the url
-    expect(current_path).to eq "/search"
-    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
 
-    # And the stations should be limited to Electric and Propane
+    fill_in "q", with: 80203
+
+    click_on "Locate"
+    
+    expect(current_path).to eq "/search"
     expect(page).to have_content("ELEC")
-    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
     expect(page).to have_content("count: 10")
   end 
 end 
