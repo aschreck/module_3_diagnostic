d48b65c6bdc56594b528127f374b45b1515b644f
Aaron Schreck <schreck.aaron@gmail.com>
2017-12-13 10:50:20 -0700

    some name refactoring, removed comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,7 @@
+
 class SearchController < ApplicationController
 
   def index
-    @stations = DevnetService.get_stations(params[:q])
-
-
-    # response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&limit=10&location=#{params[:q]}&radius=6.0&api_key=RcMCcV7bXdBRp4r5Vgh4FZR5jvwGqBJgPgfG9rQN&format=JSON")
-    # stations = JSON.parse(response.body)["fuel_stations"]#this returns a blob of JSON. 
-
-    # @stations = stations.map do |station|
-    #   Station.new(station)
-    # end 
+    @stations = DevnetService.get_nearby_stations(params[:q])
   end 
 end 
--- app/services/devnet_service.rb
+++ app/services/devnet_service.rb
@@ -2,9 +2,9 @@ require 'station'
 
 class DevnetService 
 
-  def self.get_stations(location)
+  def self.get_nearby_stations(location)
     response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=LPG,ELEC&limit=10&location=#{location}&radius=6.0&api_key=#{ENV["DEVNET_API_KEY"]}&format=JSON")
-    parsed_stations = JSON.parse(response.body)["fuel_stations"]#this returns a blob of JSON. 
+    parsed_stations = JSON.parse(response.body)["fuel_stations"]
     make_stations(parsed_stations) 
   end 
 
